13e6697128529ffc35c441f22140a9e256983d4e
LorisUnibo <loris.cangini@studio.unibo.it>
2020-01-23 17:03:41 +0100

    scafi support works!

--- app/src/main/java/it/unibo/aggregatecomputingclient/MainActivity.kt
+++ app/src/main/java/it/unibo/aggregatecomputingclient/MainActivity.kt
@@ -1,13 +1,19 @@
 package it.unibo.aggregatecomputingclient
 
+import adapters.scafi.AbstractAggregateProgram
+import adapters.scafi.ScafiAdapter
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import communication.MessageType
+import devices.VirtualDevice
 import it.unibo.aggregatecomputingclient.devices.Client
 import it.unibo.aggregatecomputingclient.devices.Server
 import kotlinx.android.synthetic.main.activity_main.*
+import server.Support
+import server.Topology
 import java.net.InetAddress
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     private lateinit var client: Client
@@ -17,6 +23,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        thread {
+//            repeat(5) {
+//                val d = Support.devices.createAndAddDevice { id ->
+//                    VirtualDevice(id).apply {
+//                        adapter = ScafiAdapter(this, object : AbstractAggregateProgram() {
+//                            override fun main(): Any = mid()
+//                        })
+//                    }
+//                }
+//            }
+//            Support.devices.finalize(Topology.Line)
+//            Support.execute()
+//        }
     }
 
     override fun onDestroy() {
