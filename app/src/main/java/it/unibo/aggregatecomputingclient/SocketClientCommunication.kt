b555c8aae2e16adc140bc67656937f70338b5edd
LorisUnibo <loris.cangini@studio.unibo.it>
2020-01-25 17:42:16 +0100

    mini var rename

--- app/src/main/java/it/unibo/aggregatecomputingclient/SocketClientCommunication.kt
+++ app/src/main/java/it/unibo/aggregatecomputingclient/SocketClientCommunication.kt
@@ -2,20 +2,20 @@ package it.unibo.aggregatecomputingclient
 
 import communication.Message
 import communication.MessageType
-import devices.client.Server
 import devices.server.RemoteDevice
 import java.net.SocketAddress
 import kotlin.concurrent.thread
 
 /**
+ * Mean of communication between this Client and the Server
  * onID: What to do when the Server tells this Client its ID
  * onMessage: What to do when this Client receives a message
  */
-class SocketClientCommunication(address: SocketAddress,
+class SocketClientCommunication(localAddress: SocketAddress,
                                 private val onID: (Int) -> Unit,
                                 private val onMessage: (Message) -> Unit) : ClientCommunication {
 
-    private val listener = RemoteDevice(-1, address)
+    private val listener = RemoteDevice(-1, localAddress)
 
     override fun subscribeToServer(server: RemoteDevice) {
         thread {
